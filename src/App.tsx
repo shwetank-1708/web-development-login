26982bbab10a7c078dd183be569e6036d1c92018
shwetank-1708 <shwetank.chauhan17@gmail.com>
2024-12-07 15:51:27 +0530

    Created Login and SingUp Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import SingUp from "./pages/SingUp";
 
 function App() {
   return (
     <>
       <Navbar />{" "}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SingUp />} />
+      </Routes>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,25 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div>
+        <ul className="flex justify-around bg-gray-400 h-11 items-center font-bold text-white">
+          <div>
+            <NavLink to="/">
+              <p>LogIn</p>
+            </NavLink>
+          </div>
+
+          <div>
+            <NavLink to="/signup">
+              <p>Sing Up</p>
+            </NavLink>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
