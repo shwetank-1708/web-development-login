92d0cdb3ee9262c90cc0862ab13b3c636676eab1
shwetank-1708 <shwetank.chauhan17@gmail.com>
2024-12-09 16:34:35 +0530

    Added Few New Files

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,17 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <form>
+        <label>Email</label>
+        <input type="email" placeholder="Enter Your Email" />
+        <br />
+        <label>Password</label>
+        <input type="text" placeholder="Enter Your Password" />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
